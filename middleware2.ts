934458f7b46195162458df99e2c82b339bcee2af
pepi88-88 <pepi88fly@gmail.com>
2025-10-04 11:28:50 +0200

    Update middleware2.ts

--- middleware2.ts
+++ middleware2.ts
@@ -1,3 +1,1 @@
-import { NextResponse } from 'next/server'
-export function middleware(req){const p=req.nextUrl.pathname; const admin=p.startsWith('/admin'); const prot=p.startsWith('/api')&&!p.startsWith('/api/public')&&!p.startsWith('/api/auth'); if(admin||prot){const s=req.cookies.get('admin_session')?.value; if(s!=='1'){ if(prot) return NextResponse.json({error:'Unauthorized'},{status:401}); const u=req.nextUrl.clone(); u.pathname='/login-staff'; return NextResponse.redirect(u) }} return NextResponse.next() }
-export const config={ matcher:['/admin/:path*','/api/:path*']}
+
