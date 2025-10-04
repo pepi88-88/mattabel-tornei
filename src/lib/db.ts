6d13cf9b0d0bf658d0cb3c666b2e3dd7d6ce56b7
pepi88-88 <pepi88fly@gmail.com>
2025-10-04 11:44:33 +0200

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,35 +1,9 @@
-// src/lib/db.ts
-import { Pool } from 'pg'
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-})
-
-// Query semplice
-async function query(text: string, params?: any[]) {
-  return pool.query(text, params)
-}
-
-// Transazione garantendo la stessa connessione
-async function tx<T>(fn: (c: any) => Promise<T>) {
-  const client = await pool.connect()
-  try {
-    await client.query('BEGIN')
-    const res = await fn(client)
-    await client.query('COMMIT')
-    return res
-  } catch (e) {
-    try { await client.query('ROLLBACK') } catch {}
-    throw e
-  } finally {
-    client.release()
+// src/types/pg.d.ts
+declare module 'pg' {
+  export class Pool {
+    constructor(config?: any)
+    query(text: string, params?: any[]): Promise<any>
+    connect(): Promise<any>
   }
+  export type PoolClient = any
 }
-
-// Compat: per codice che fa `const client = await db.connect()`
-async function connect() {
-  return pool.connect()
-}
-
-export const db = { query, tx, connect }
